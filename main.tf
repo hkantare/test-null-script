f7504b2102f6779fae7f394aee464434c011874c
HARINI KANTAREDDY <hkantare@in.ibm.com>
2022-12-19 13:02:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,15 @@ resource "null_resource" "health_check" {
     command = "./helloworld.sh"
   }
 }
+
+
+resource "null_resource" "actions" {
+  provisioner "local-exec" {
+    command = "/bin/bash scripts/zip_cf.sh"
+
+    environment = {
+      FILENAME = "test"
+      FILEPATH = "."
+    }
+  }
+}
